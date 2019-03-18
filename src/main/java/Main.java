5139832785f89e3eba7d4c5668a38e142b0e4813
Big Big <bilgr@mail.ru>
2019-03-19 02:34:37 +0300

    Almost done 1st task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello wrld");
+        new Tictactoe();
+        Tictactoe.show();
+        Tictactoe.addSymb('O', 1, 1);
+        Tictactoe.addSymb('O', 2, 1);
+        Tictactoe.addSymb('O', 3, 1);
+        Tictactoe.addSymb('O', 2, 2);
+        Tictactoe.addSymb('O', 1, 2);
+        Tictactoe.addSymb('O', 1, 3);
+        Tictactoe.show();
+        System.out.println(Tictactoe.lngstSeq());
     }
 }
--- src/main/java/Tictactoe.java
+++ src/main/java/Tictactoe.java
@@ -1,3 +1,64 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static sun.swing.MenuItemLayoutHelper.max;
+
 public class Tictactoe {
-    // coming soon...
+
+    static private char[][] Grid;
+    static private final ArrayList<Character> possibleSymbols = new ArrayList<Character>(Arrays.asList('X', 'O'));
+
+    Tictactoe() {
+        Grid = new char[][]{
+                {'_', '_', '_'},
+                {'_', '_', '_'},
+                {'_', '_', '_'},
+        };
+    }
+
+    static void addSymb(char a, int x, int y){
+        if ((x > 0) && (x < 4) && (y > 0) && (y < 4) && possibleSymbols.contains(a)) Grid[x - 1][y - 1] = a;
+        else System.out.println("Неверный ввод");
+    }
+
+    public static void delSymb(int x, int y){
+        if ((x > 0) && (x < 4) && (y > 0) && (y < 4)) Grid[x - 1][y - 1] = '_';
+    }
+
+    static int lngstSeq(){
+        int rez = 0;
+        int temprez = 0;
+        for (int i = 0; i < Grid.length; i++) {
+            for (int j = 0; j < Grid[i].length; j++){
+                if (possibleSymbols.contains(Grid[i][j]) && temprez == 0) temprez = 1;
+                if (j < (Grid[i].length - 1) && Grid[i][j] == Grid[i][j+1] && possibleSymbols.contains(Grid[i][j])) temprez++;
+                else {
+                    rez = max(temprez, rez);
+                    temprez = 0;
+                }
+            }
+        }
+        for (int j = 0; j < Grid[0].length; j++) {
+            for (int i = 0; i < Grid.length; i++){
+                if (possibleSymbols.contains(Grid[i][j]) && temprez == 0) temprez = 1;
+                if (i < (Grid.length - 1) && Grid[i][j] == Grid[i+1][j] && possibleSymbols.contains(Grid[i][j])) temprez++;
+                else {
+                    rez = max(temprez, rez);
+                    temprez = 0;
+                }
+            }
+        }
+        return rez;
+    }
+
+    static void show(){
+        for (int i = 0; i < Grid.length; i++) {
+            for (int j = 0; j < Grid[0].length; j++){
+                System.out.print(Grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
